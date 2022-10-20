4cc65cae1e8cd07f5ae389515b1f197afd0643dd
Kosmonaut 3173 <re.huerta3173@gmail.com>
2022-10-20 00:57:19 -0500

    added needed consts to server file

--- server.js
+++ server.js
@@ -1,14 +1,26 @@
 const express = require('express');
+const path = require('path');
+const api = require('./routes/index');
 const app = express();
 
 const PORT = process.env.PORT || 5000;
 
+// middleware
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(express.static('public'));
 
 app.use('/api',api);
 app.use('/',html);
+
+// GET rout hor home
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname), './public/index.html')
+})
+// GET route for notes
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'))
+})
 
 app.listen(PORT, ()=>{
     console.log(`server running on port ${PORT}`)
